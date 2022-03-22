c2ac5920b9e229657a93f4518eb861038baef67b
ayushman <unixparadoxfb@gmail.com>
2022-03-22 14:51:58 +0545

    checking databases and tables

--- populateDB.py
+++ populateDB.py
@@ -1,7 +1,6 @@
 #establishing a database connection
-from getpass import getpass
 from django.db import connection
-from mysql.connector import connect, Error
+from mysql.connector import connect, Error, cursor
 
 
 CONS_HOST = "localhost"
@@ -9,6 +8,7 @@ CONS_USER = "root"
 CONS_PASS = ""
 
 
+#connecting with database
 try:
     mydb = connect(
         host=CONS_HOST,
@@ -18,5 +18,20 @@ try:
     print(mydb)
 except Error as e:
     print(e)
+
+
+#checking the available databases, tables
+show_db_query = "show databases;"
+select_db_query = "use booking_pro;"
+show_table_query = "show tables;"
+
+myCursor = mydb.cursor()
+myCursor.execute(select_db_query)
+myCursor.execute(show_table_query)
+myResult = myCursor.fetchall()
+
+for x in myResult:
+    print(x)
+
 
 
