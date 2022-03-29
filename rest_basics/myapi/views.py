d8bc31d7550d12820dd34a8ad95c1035b58470f7
ayushmanadhikari <unixparadoxfb@gmail.com>
2022-03-29 10:45:37 +0545

    complete

--- rest_basics/myapi/views.py
+++ rest_basics/myapi/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .serializers import HeroSerializer
+from .models import Hero
+from rest_framework import viewsets
+
+
+class HeroViewSet(viewsets.ModelViewSet):
+    queryset = Hero.objects.all().order_by('name')
+    serializer_class = HeroSerializer
+
+    
